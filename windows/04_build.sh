ab51da5fc73f2c78ee52ed50956bc31c4732c2aa
okayu <moka1988jp@gmail.com>
2019-04-06 16:26:30 +0900

    update

--- windows/04_build.sh
+++ windows/04_build.sh
@@ -3,10 +3,10 @@ rm -rf ./build
 mkdir -p ./build/vimfiles
 
 # メッセージファイルを生成
-python3 C:/Python36/tools/i18n/msgfmt.py ./po/ja.po
+python3 C:/Python37/tools/i18n/msgfmt.py ./po/ja.po
 
 # ビルド（CUI）
-mingw32-make -f Make_ming.mak  \
+make -f Make_ming.mak  \
   GUI=no \
   ARCH=x86-64 \
   IME=yes \
@@ -14,22 +14,22 @@ mingw32-make -f Make_ming.mak  \
   ICONV=yes \
   FEAT_TERMGUICOLORS=yes \
   clean
-mingw32-make -f Make_ming.mak \
+make -f Make_ming.mak \
   ARCH=x86-64 \
   IME=yes \
   MBYTE=yes \
   ICONV=yes \
   FEAT_TERMGUICOLORS=yes \
-  PERL=C:/Strawberry/perl DYNAMIC_PERL=yes PERL_VER=526 \
-  PYTHON=C:/tools/python2 DYNAMIC_PYTHON=yes PYTHON_VER=27 \
-  PYTHON3=C:/python36 DYNAMIC_PYTHON3=yes PYTHON3_VER=36 \
-  RUBY=C:/tools/ruby23 DYNAMIC_RUBY=yes RUBY_VER=23 RUBY_API_VER_LONG=2.3.0 \
+  PERL=C:/Strawberry/perl DYNAMIC_PERL=yes PERL_VER=528 \
+  PYTHON=C:/Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 \
+  PYTHON3=C:/Python37 DYNAMIC_PYTHON3=yes PYTHON3_VER=37 \
+  RUBY=C:/tools/ruby25 DYNAMIC_RUBY=yes RUBY_VER=25 RUBY_API_VER_LONG=2.5.0 \
   LUA=~/src/vim/lua-5.3.3/src DYNAMIC_LUA=yes LUA_VER=53 \
   GUI=no
 cp vim.exe ./build/
 
 # ビルド（GUI）
-mingw32-make -f Make_ming.mak  \
+make -f Make_ming.mak  \
   GUI=yes \
   ARCH=x86-64 \
   IME=yes \
@@ -37,16 +37,16 @@ mingw32-make -f Make_ming.mak  \
   ICONV=yes \
   FEAT_TERMGUICOLORS=yes \
   clean
-mingw32-make -f Make_ming.mak \
+make -f Make_ming.mak \
   ARCH=x86-64 \
   IME=yes \
   MBYTE=yes \
   ICONV=yes \
   FEAT_TERMGUICOLORS=yes \
-  PERL=C:/Strawberry/perl DYNAMIC_PERL=yes PERL_VER=526 \
-  PYTHON=C:/tools/python2 DYNAMIC_PYTHON=yes PYTHON_VER=27 \
-  PYTHON3=C:/python36 DYNAMIC_PYTHON3=yes PYTHON3_VER=36 \
-  RUBY=C:/tools/ruby23 DYNAMIC_RUBY=yes RUBY_VER=23 RUBY_API_VER_LONG=2.3.0 \
+  PERL=C:/Strawberry/perl DYNAMIC_PERL=yes PERL_VER=528 \
+  PYTHON=C:/Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 \
+  PYTHON3=C:/Python37 DYNAMIC_PYTHON3=yes PYTHON3_VER=37 \
+  RUBY=C:/tools/ruby25 DYNAMIC_RUBY=yes RUBY_VER=25 RUBY_API_VER_LONG=2.5.0 \
   LUA=~/src/vim/lua-5.3.3/src DYNAMIC_LUA=yes LUA_VER=53 \
   GUI=yes \
   STATIC_STDCPLUS=yes \
@@ -62,10 +62,10 @@ cp C:/msys64/mingw64/bin/libwinpthread-1.dll ./build/
 cp C:/msys64/usr/bin/winpty.dll ./build/
 cp C:/msys64/usr/bin/winpty-agent.exe ./build/
 
-cp C:/Strawberry/perl/bin/perl526.dll ./build/
-cp C:/tools/ruby23/bin/x64-msvcrt-ruby230.dll ./build/
+cp C:/Strawberry/perl/bin/perl528.dll ./build/
+cp C:/tools/ruby25/bin/x64-msvcrt-ruby250.dll ./build/
 cp C:/Windows/System32/python27.dll ./build/
-cp C:/Python36/python36.dll ./build/
+cp C:/Python37/python37.dll ./build/
 cp C:/ProgramData/chocolatey/lib/lua53/tools/lua53.dll ./build/
 cp -r ../runtime ./build/vim80
 mkdir -p ./build/vim80/lang/ja/LC_MESSAGES
